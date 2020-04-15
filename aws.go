ded29408932bae819c5870a365f8aeec3fba41e6
Anthony Gargiulo <anthony@agargiulo.com>
2020-04-14 18:18:02 -0700

    Initial code moved from other projects. AwsSession and a few clients

---
+++ aws.go
@@ -0,0 +1,29 @@
+package common_lib
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/aws/aws-sdk-go/service/lambda/lambdaiface"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/aws/aws-sdk-go/service/ssm/ssmiface"
+)
+
+func SSMClient(region string) ssmiface.SSMAPI {
+	return ssm.New(AwsSession(region))
+}
+
+func LambdaClient(region string) lambdaiface.LambdaAPI {
+	return lambda.New(AwsSession(region))
+}
+
+func AwsSession(region string) *session.Session {
+	sessionOpts := session.Options{
+		Config: aws.Config{
+			Region: aws.String(region),
+		},
+		SharedConfigState: session.SharedConfigEnable,
+	}
+	awsSession := session.Must(session.NewSessionWithOptions(sessionOpts))
+	return awsSession
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module src.doom.fm/schism/common-lib
+
+go 1.14
+
+require github.com/aws/aws-sdk-go v1.30.7
